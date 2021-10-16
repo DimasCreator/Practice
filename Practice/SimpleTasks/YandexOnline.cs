aca3dbc3d104db5de09416df67aca18f19325def
Dima Kulakov <d.kulakov@team21.ru>
2021-10-16 16:45:26 +0300

    Start Yandex

--- Practice/SimpleTasks/YandexOnline.cs
+++ Practice/SimpleTasks/YandexOnline.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Practice.SimpleTasks
 {
+    /// <summary>
+    /// Задачи онлайн собеседования Яндекс на позицию стажера.
+    /// </summary>
     public class YandexOnline
     {
+        public static string Task1(int n)
+        {
+            return "";
+        }
+
+        public static string Task2(int[] array)
+        {
+            return "";
+        }
+
+        public static int Task3(string str)
+        {
+            return 0;
+        }
     }
 }
